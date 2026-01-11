673485d3b308cdf239d51ede2dc38551bac24faa
ANITBIOTICS <tianaigu0902@gmail.com>
2026-01-11 08:19:04 -0800

    cleared basic dp files

--- Dynamic Programming/fibonnaci.cpp
+++ Dynamic Programming/fibonnaci.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n){
+int fib1(int n){
     if(n == 1){
         return 1;
     }else if (n == 0){
         return 0;
     }
 
-    return fib(n-1) + fib(n-2);
+    return fib1(n-1) + fib1(n-2);
+}
+
+int fib2(int n){
+    vector<int> dp (n);
+    
 }
 
 int main(){
     int n;
     cin >> n;
-    int num = fib(n);
+    int num = fib1(n);
     cout << num;
     return 0;
 }
