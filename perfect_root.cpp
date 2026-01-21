4ce7ebefcf5e4435f1dbc8911dfd44823b4052c1
ANITBIOTICS <tianaigu0902@gmail.com>
2026-01-20 20:25:46 -0800

    Included the k=1 case in 2026 Bronze Cow Splits question. Coded version 1 of Perfect_Root (Code is yet to be run and checked)

---
+++ perfect_root.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        int temp;
+        for(int i = 0; i < n; i++){
+            cout << temp << " ";
+            temp ++;
+        }
+    }
+    return 0;
+}
