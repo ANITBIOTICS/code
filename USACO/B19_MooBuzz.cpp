8253e705d881d15f9d2dae71def01b8ea4570670
ANITBIOTICS <tianaigu0902@gmail.com>
2026-01-25 08:23:39 -0800

    empty file for MooBuzz bronze 2019 q

---
+++ USACO/B19_MooBuzz.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+
+    int N;
+    cin >> N;
+
+    /*
+    let the theoretical number of things said be M.
+    M = N + M/5 + M/3 - M%15
+    
+    */
+
+    return 0;
+}
