a09d3ef189498f0358e113e18160fc57cf950ff6
ANITBIOTICS <tianaigu0902@gmail.com>
2026-01-25 08:41:29 -0800

    coded bronze 2019 quesiton MooBuzz (yet to be tested on website)

--- USACO/B19_MooBuzz.cpp
+++ USACO/B19_MooBuzz.cpp
@@ -12,7 +12,12 @@ int main(){
     let the theoretical number of things said be M.
     M = N + M/5 + M/3 - M%15
     
+    1, 2, moo, 4, moo, moo, 7,8, moo, moo, 11, moo, 13, 14, moo, 
+    1-15 = 7 moos, 8 numbers
     */
 
+    vector<int> numMoo = {1, 2, 4, 7, 8, 11, 13, 14};
+    int num = (N-1)/8 * 15 + numMoo[(N-1)%8];
+    cout << num;
     return 0;
 }
