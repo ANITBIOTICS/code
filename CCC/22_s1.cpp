cba10ae140b83ca0bd8f024a42fc7347e49f6357
ANITBIOTICS <tianaigu0902@gmail.com>
2026-02-15 08:40:42 -0800

    coded 2022 s1

--- CCC/22_s1.cpp
+++ CCC/22_s1.cpp
@@ -1,10 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int s1(long long N){
+    if(N < 4)return 0; 
+    else if (N == 5 || N == 4) return 1;
+    
+    long long ans = 0;
+    for (long long b = 0; 5 * b <= N; b++) {
+        long long r = N - 5 * b;
+        if (r % 4 == 0) ans++;
+    }
+
+    return ans;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr); 
     
-
+    long long N;
+    cin >> N;
+    cout << s1(N);
     return 0;
 }
